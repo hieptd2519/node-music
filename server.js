9acd9ddff6cdd9f2e4644e3d9bf91b2bad4b4b68
hieptd <hiep.td@zinza.com.vn>
2022-12-08 17:13:16 +0700

    a

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 import express from "express";
 import FFplay from "./src/FFplay/index.js";
+import ControlRouter from "./src/routers/control.js";
 
 const app = express();
 const port = 5000;
-const player = new FFplay(
-  "./src/downloads/MẶT_MỘC___Phạm_Nguyên_Ngọc_x_VAnh_x_Ân_Nhi_Or.mp3"
-);
-//test 2
+
+const listSongPaths = [
+  "./src/downloads/file_example_MP3_700KB.mp3",
+  "./src/downloads/MẶT_MỘC___Phạm_Nguyên_Ngọc_x_VAnh_x_Ân_Nhi_Or.mp3",
+];
+
+app.use("/control", ControlRouter);
 
 app.get("/", (req, res) => {
-  player.running = true;
-
   res.send("Hello World!");
 });
-setTimeout(() => {
-  player.pause();
-  console.log("pause");
-}, 12000);
-setTimeout(() => {
-  player.resume();
-  console.log("resume");
-}, 15000);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
